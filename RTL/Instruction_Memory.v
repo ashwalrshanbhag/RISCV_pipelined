6631094a83002f93b827a350139b076539d46805
Ashwal  R Shanbhag <184464884+ashwalrshanbhag@users.noreply.github.com>
2026-06-01 10:09:16 +0530

    Refactor Instruction_Memory module structure
    Updated module syntax and added comments for clarity.

--- RTL/Instruction_Memory.v
+++ RTL/Instruction_Memory.v
@@ -6,11 +6,11 @@ module Instruction_Memory(
 
     reg [31:0] mem [1023:0];
     
-    // Using a clear 32-bit hex literal prevents replication syntax errors
+    
     assign RD = (rst == 1'b0) ? 32'h00000000 : mem[A[31:2]];
 
     initial begin
-        // Using full directory path with forward slashes for Vivado compatibility
+       //file containing instructions 
         $readmemh("F:/Downloads/myproject/myproject/RISCV_final/program.hex", mem);
     end
 
